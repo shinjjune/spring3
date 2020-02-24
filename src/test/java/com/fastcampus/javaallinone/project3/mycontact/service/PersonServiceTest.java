e52b229524fc6cbce4991ee2c69214917c83fb2e
shinjs <sjh8236@naver.com>
2020-02-24 22:51:31 +0900

    spring3 project

---
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -0,0 +1,92 @@
+package com.fastcampus.javaallinone.project3.mycontact.service;
+
+import com.fastcampus.javaallinone.project3.mycontact.domain.Block;
+import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
+import com.fastcampus.javaallinone.project3.mycontact.repository.BlockRepository;
+import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
+import jdk.vm.ci.meta.Local;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class PersonServiceTest {
+    @Autowired
+    private PersonService personService;
+    @Autowired
+    private PersonRepository personRepository;
+    @Autowired
+    private BlockRepository blockRepository;
+
+    @Test
+    void getPeopleExcludeBlocks() {
+        givenPeople();
+
+        List<Person> result = personService.getPeopleExcludeBlocks();
+
+        result.forEach(System.out::println);
+    }
+
+    @Test
+    void cascadeTest() {
+        givenPeople();
+
+        List<Person> result = personRepository.findAll();
+        result.forEach(System.out::println);
+
+        Person person = result.get(3);
+        person.getBlock().setStartDate(LocalDate.now());
+        person.getBlock().setEndDate(LocalDate.now());
+
+        personRepository.save(person);
+        personRepository.findAll().forEach(System.out::println);
+
+//        personRepository.delete(person);
+//        personRepository.findAll().forEach(System.out::println);
+//        blockRepository.findAll().forEach(System.out::println);
+
+        person.setBlock(null);
+        personRepository.save(person);
+        personRepository.findAll().forEach(System.out::println);
+        blockRepository.findAll().forEach(System.out::println);
+    }
+
+    @Test
+    void getPerson() {
+        givenPeople();
+
+        Person person = personService.getPerson(3L);
+
+        System.out.println(person);
+    }
+
+    private void givenPeople() {
+        givenPerson("shinjjune", 10, "B");
+        givenPerson("sungwon", 28, "AB");
+        givenBlockPerson("hando", 86, "A");
+        givenBlockPerson("shinjjune", 17, "B");
+    }
+
+//    private void givenBlocks() {
+//        givenBlock("hando");
+//    }
+
+    private void givenPerson(String name, int age, String bloodType) {
+        personRepository.save(new Person(name, age, bloodType));
+    }
+
+    private void givenBlockPerson(String name, int age, String bloodType) {
+        Person blockPerson = new Person(name, age, bloodType);
+        blockPerson.setBlock(new Block(name));
+
+        personRepository.save(blockPerson);
+    }
+    private Block givenBlock(String name) {
+        return blockRepository.save(new Block(name));
+    }
+
+}
