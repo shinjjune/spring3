473e5ceb5d05cb49142cd35be9318fbb25545ef2
shinjs <sjh8236@naver.com>
2020-02-24 22:54:22 +0900

    spring3 project

---
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.fastcampus.javaallinone.project3.mycontact.repository;
+
+import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class PersonRepositoryTest {
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void crud() {
+        Person person = new Person();
+        person.setName("shinjjune");
+        person.setAge(25);
+        person.setBloodType("B");
+
+        personRepository.save(person);
+
+        System.out.println(personRepository.findAll());
+
+        List<Person> people = personRepository.findAll();
+
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("shinjjune");
+        assertThat(people.get(0).getAge()).isEqualTo(25);
+        assertThat(people.get(0).getBloodType()).isEqualTo("B");
+    }
+    @Test
+    void hashCodeAndEquals() {
+        Person person1 = new Person("shinjjune", 25,"B");
+        Person person2 = new Person("shinjjune", 25,"B");
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+
+        Map<Person, Integer> map = new HashMap<>();
+        map.put(person1, person1.getAge());
+
+        System.out.println(map);
+        System.out.println(map.get(person2));
+    }
+
+
+}
