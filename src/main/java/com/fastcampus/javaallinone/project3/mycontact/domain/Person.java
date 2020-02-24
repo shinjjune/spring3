b5cb2dc8b22ee972d0da29a7244bd695cf7d1a56
shinjs <sjh8236@naver.com>
2020-02-24 23:01:38 +0900

    spring3 project

---
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -0,0 +1,42 @@
+package com.fastcampus.javaallinone.project3.mycontact.domain;
+
+import jdk.vm.ci.meta.Local;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+public class Person {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private int age;
+
+    private String hobby;
+
+    @NonNull
+    private String bloodType;
+
+    private String address;
+
+    private LocalDate birthday;
+
+    private String job;
+
+    @ToString.Exclude
+    private String phoneNumber;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    private Block block;
+}
